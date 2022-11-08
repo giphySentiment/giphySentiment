5649e58e548751fd5b57acb9e8be2459ef1b25b3
chriskimty <christaeyoung.kim@gmail.com>
2022-11-08 15:33:35 -0500

    restructured all components and overall cleanup

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,36 +1,33 @@
-import { useEffect, useState } from "react";
-
-
 const Forms = (props) => {
-
-    const date = new Date()
+    // Variables to set date info
+        // NB: We could pass down each of these as a variable into results so that it doesn't need to be repeated information on the results page
+    const date = new Date();
     const month = date.toLocaleString('en-US', {
         month: 'long',
     });
-    const day = date.getDate()
+    const day = date.getDate();
     const today = new Date().toLocaleString(
         'default', { weekday: 'long' }
     );
 
-    const [test, setTest] = useState([])
-
-
+    // Function to change setMood to user input value
     const handleMoodChange = (e) => {
-        props.setMood(e.target.value)
+        props.setMood(e.target.value);
     }
 
     return (
-        <section>
-                <h2>It's {`${month} ${day}`}</h2>
-                <h2>Happy {`${today}`}! How are you feeling?</h2>
+        <section className="forms">
+            {/* N.B: We don't actually need to display the month and day, but it will be collected in the background */}
+            <h2>It's {`${month} ${day}`}</h2>
+            <h2>Happy {`${today}`}! How are you feeling?</h2>
             <form onSubmit={(e) => props.handleFormSubmit(e, props.mood)}>
-               
-                <label htmlFor="moodSelector" className="visuallyHidden">Type in an emotion</label>
+                <label htmlFor="moodSelector"
+                    // add visuallyHidden class in CSS
+                    className="visuallyHidden">Type in an emotion</label>
                 <input name="moodSelector" id="moodSelector" type="text"
                  onChange={(event) => handleMoodChange(event)}
                 />
-
-                <button >find gifs!</button>
+                <button className="buttonContainer">find gifs!</button>
             </form>
         </section>
     )
---
+++ src/components/Timeline.js
@@ -0,0 +1,47 @@
+import firebaseConfig from '../firebase';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { useEffect, useState } from "react";
+import uuid from "react-uuid";
+
+const Timeline = (props) => {
+
+    // State to save user's gif+info object into the timeline
+    const [timeline, setTimeline] = useState([]);
+
+    useEffect(() => {
+        const database = getDatabase(firebaseConfig);
+        const databaseRef = ref(database);
+
+        onValue(databaseRef, (response) => {
+            const newState = [];
+            const data = response.val();
+
+            for (let key in data) {
+                newState.push({ key: key, name: data[key] })
+            }
+            setTimeline(newState);
+        })
+    }, []);
+
+    return (
+        <div>
+            <ul>
+                {timeline.map((result, index) => {
+                    return (
+                        <div
+                            className="savedToTimeline"
+                            key={uuid()}>
+                            <h2>{`On ${result.name.date}, you felt: ${result.name.mood}`}</h2>
+                            <img
+                                src={result.name.image}
+                                alt={`user selected gif to show the mood of ${result.name.mood}`}
+                            />
+                        </div>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default Timeline;
